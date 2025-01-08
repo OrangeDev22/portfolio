3eaf96280801b6b7515fb43f2217cc1870935686
Gabriel Matute <orangedev8@gmail.com>
2025-01-08 14:39:16 -0600

    fixing typo in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { motion } from 'motion/react'
+import React from "react";
+import { motion } from "motion/react";
 
 const Footer = () => {
   return (
     <motion.footer
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
-      className='text-white py-4 text-center'
+      className="text-white py-4 text-center"
     >
-      <div className='container mx-auto'>
-        <p className='mb-2'>
-          Coded in <span className='text-violet-300'>Visual Studio Code</span>{' '}
+      <div className="container mx-auto">
+        <p className="mb-2">
+          Coded in <span className="text-violet-300">Visual Studio Code</span>{" "}
           by yours trully using:
-          <span className='font-semibold'>
-            {' '}
-            <span className='text-violet-300'>React, Motion</span> and{' '}
-            <span className='text-violet-300'>TailwindCSS</span>
+          <span className="font-semibold">
+            {" "}
+            <span className="text-violet-300">React, Motion</span> and{" "}
+            <span className="text-violet-300">TailwindCSS</span>.
           </span>
         </p>
       </div>
     </motion.footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
