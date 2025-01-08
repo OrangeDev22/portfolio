5314b3c568360363d7cb2438e874dc8a709c8bef
Gabriel Matute <orangedev8@gmail.com>
2025-01-08 14:16:44 -0600

    adding animated wrapper to project section

--- src/components/ProjectShowcase/index.jsx
+++ src/components/ProjectShowcase/index.jsx
@@ -1,58 +1,55 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'
-import { motion } from 'motion/react'
-import { projects } from '../../utils'
-import AnimatedTitle from '../AnimatedTitle'
-import Container from '../Container'
+import React from "react";
+import PropTypes from "prop-types";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import { motion } from "motion/react";
+import { projects } from "../../utils";
+import AnimatedTitle from "../AnimatedTitle";
+import Container from "../Container";
+import AnimatedWrapper from "../AnimatedWrapper";
 
 const ProjectShowcase = () => {
   return (
-    <Container id='projects'>
-      <AnimatedTitle>Projects</AnimatedTitle>{' '}
-      <div className='max-w-5xl flex flex-col gap-10'>
+    <Container id="projects">
+      <AnimatedTitle>Projects</AnimatedTitle>{" "}
+      <div className="max-w-5xl flex flex-col gap-10">
         {projects.map((project, index) => (
-          <motion.div
+          <AnimatedWrapper
             key={index}
             className={`flex flex-col md:flex-row items-center gap-6 p-4 rounded-md transition-colors duration-300 ${
-              index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'
+              index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
             } hover:bg-slate-800 hover:shadow-md`}
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, ease: 'easeOut' }}
-            viewport={{ once: true }}
           >
             {/* Image Section */}
-            <div className='w-full md:w-1/2'>
+            <div className="w-full md:w-1/2">
               <motion.img
                 src={project.image}
                 alt={project.title}
-                className='w-full h-auto max-w-[800px] max-h-[500px] rounded-md object-cover'
+                className="w-full h-auto max-w-[800px] max-h-[500px] rounded-md object-cover"
                 whileHover={{ scale: 1.05 }}
-                transition={{ duration: 0.3, ease: 'easeOut' }}
+                transition={{ duration: 0.3, ease: "easeOut" }}
               />
             </div>
 
             {/* Content Section */}
-            <div className='w-full md:w-1/2 space-y-4'>
-              <h2 className='text-2xl font-semibold'>{project.title}</h2>
-              <p className='text-gray-400'>{project.description}</p>
+            <div className="w-full md:w-1/2 space-y-4">
+              <h2 className="text-2xl font-semibold">{project.title}</h2>
+              <p className="text-gray-400">{project.description}</p>
               <div>
-                <strong className='text-white'>Skills:</strong>{' '}
-                <span className='text-violet-200'>
-                  {project.skills.join(', ')}.
+                <strong className="text-white">Skills:</strong>{" "}
+                <span className="text-violet-200">
+                  {project.skills.join(", ")}.
                 </span>
               </div>
-              <div className='flex gap-4'>
+              <div className="flex gap-4">
                 {project.demoUrl && (
                   <motion.a
                     href={project.demoUrl}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='flex items-center gap-2 px-4 py-2 text-white bg-violet-400 rounded-md transition'
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 px-4 py-2 text-white bg-violet-400 rounded-md transition"
                     whileHover={{ scale: 1.1, rotate: 2 }}
                     whileTap={{ scale: 0.95 }}
-                    transition={{ duration: 0.1, ease: 'easeOut' }}
+                    transition={{ duration: 0.1, ease: "easeOut" }}
                   >
                     <FaExternalLinkAlt />
                     Demo
@@ -60,23 +57,23 @@ const ProjectShowcase = () => {
                 )}
                 <motion.a
                   href={project.githubUrl}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='flex items-center gap-2 px-4 py-2 text-white bg-[#212830] rounded-md transition'
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-2 px-4 py-2 text-white bg-[#212830] rounded-md transition"
                   whileHover={{ scale: 1.1, rotate: 2 }}
                   whileTap={{ scale: 0.95 }}
-                  transition={{ duration: 0.1, ease: 'easeOut' }}
+                  transition={{ duration: 0.1, ease: "easeOut" }}
                 >
                   <FaGithub />
                   GitHub Code
                 </motion.a>
               </div>
             </div>
-          </motion.div>
+          </AnimatedWrapper>
         ))}
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default ProjectShowcase
+export default ProjectShowcase;
